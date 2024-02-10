b2d329ea099d93bec34b323702a436f2065986b7
abdurahmon999 <141444557+abdurahmon999@users.noreply.github.com>
2024-02-10 13:08:35 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+        const TelegramBot = require('node-telegram-bot-api');
+
+        // Ваш токен от Bot Father
+        const token = '6932732559:AAFCjolv5ZpvcWVjx1l6Fr41me3VnRTCXJY';
+
+        // Создаем экземпляр бота
+        const bot = new TelegramBot(token, { polling: true });
+
+        // Обработчик команды /start
+        bot.onText(/\/start/, (msg) => {
+        const chatId = msg.chat.id;
+        const firstName = msg.from.first_name;
+        bot.sendMessage(chatId, `Salom ${firstName} men "eho" botman`);
+        });
+
+        // Обработчик текстовых сообщений
+        bot.on('message', (msg) => {
+        const chatId = msg.chat.id;
+        const message = msg.text;
+
+        // Отправляем обратно полученное сообщение
+        bot.sendMessage(chatId, message);
+        });
+
+        console.log("Bot Muvofaqiyatli ichga tushdi!");
